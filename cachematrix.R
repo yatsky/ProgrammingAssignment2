c6206219ca92cefcf6931de3f37e0924aca2b467
Thomas Wang <wyatsky@gmail.com>
2015-04-17 09:12:43 +0800

    this should work

--- cachematrix.R
+++ cachematrix.R
@@ -9,11 +9,15 @@
 makeCacheMatrix <- function(x = matrix()) {
         i <- NULL
         set <- function(y) {
-                x <<- y
-                i <<- NULL
+                x <<- y  ## Here the x is the x in 'x = matrix()'
+                i <<- NULL  ## Here the i is the i in 'i  <- NULL'
         }
         get <- function() x
-        setinverse <- function(inverse) i <<-solve(x)
+        ## In the following line, actually we can write like 'i << -inverse'
+        ## and in the console we can use it like mx$setinverse(solve(mymatrix))
+        ## Or, we can just write i <<- solve(x)
+        ## and in the console we use it like mx$setinverse(mymatrix)
+        setinverse <- function(inverse) i <<- inverse
         getinverse <- function() i
         list(set = set, get = get,
              setinverse = setinverse,
@@ -23,7 +27,7 @@ makeCacheMatrix <- function(x = matrix()) {
 
 ## Write a short comment describing this function
 
-cacheSolve <- function(x, ...) {
+cacheSolve <- function(x, ...) {  ## Here the x is merely a parameter indicating here needs some input
         ## Return a matrix that is the inverse of 'x'
         i <- x$getinverse()
         if(!is.null(i)) {
